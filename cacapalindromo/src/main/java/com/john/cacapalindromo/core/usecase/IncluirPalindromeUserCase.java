71e0ab6ddd053ac6295a62a8a4aa15ba40a4449e
johnformata <johnvianaluz@gmail.com>
2024-02-28 10:49:40 -0300

    feat: "interface IncluirPalindromeUserCase criada para se conectar com ambiente externo"

---
+++ cacapalindromo/src/main/java/com/john/cacapalindromo/core/usecase/IncluirPalindromeUserCase.java
@@ -0,0 +1,8 @@
+package com.john.cacapalindromo.core.usecase;
+
+import com.john.cacapalindromo.core.domain.Palindrome;
+
+public interface IncluirPalindromeUserCase {
+
+    void incluir(Palindrome palindrome);
+}
