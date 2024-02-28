5d8a5ae3906044481e84d3165090af91927d9f59
johnformata <johnvianaluz@gmail.com>
2024-02-28 10:42:04 -0300

    feat: "entity palindrome criada no core"

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -5,3 +5,7 @@ F
 cacapalindromo/pom.xml,d\c\dca278ed1732e7b269a7dba66aafd4f7867b638e
 É
 Scacapalindromo/src/main/java/com/john/cacapalindromo/CacapalindromoApplication.java,9\1\91f54668308ec2a57840291b50a224fd8954813b
+Ä
+Pcacapalindromo/src/main/java/com/john/cacapalindromo/core/domain/Palindrome.java,6\3\636c92db24d70d53e2c986b78b2bd47e5de6b89e
+Å
+Q.idea/sonarlint/securityhotspotstore/6/3/636c92db24d70d53e2c986b78b2bd47e5de6b89e,0\5\05037c91ead81218d5790a6474733a1e86ef1345
---
+++ cacapalindromo/src/main/java/com/john/cacapalindromo/core/domain/Palindrome.java
@@ -0,0 +1,31 @@
+package com.john.cacapalindromo.core.domain;
+
+public class Palindrome {
+
+    private String id;
+
+    private String polindrome;
+
+    public Palindrome(){}
+
+    public Palindrome(String id, String polindrome) {
+        this.id = id;
+        this.polindrome = polindrome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPolindrome() {
+        return polindrome;
+    }
+
+    public void setPolindrome(String polindrome) {
+        this.polindrome = polindrome;
+    }
+}
