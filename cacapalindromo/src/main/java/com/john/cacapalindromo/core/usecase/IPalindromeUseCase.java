dcd5b15101ae9b2763b5975a9c7265af030393d2
johnformata <johnvianaluz@gmail.com>
2024-02-29 12:50:15 -0300

    feat: "refatorando interface UseCase usecase"

---
+++ cacapalindromo/src/main/java/com/john/cacapalindromo/core/usecase/IPalindromeUseCase.java
@@ -0,0 +1,15 @@
+package com.john.cacapalindromo.core.usecase;
+
+import com.john.cacapalindromo.core.domain.Palindromo;
+
+import java.util.List;
+/**
+ * @author john
+ */
+public interface IPalindromeUseCase {
+
+    List<String> findAndSavePalindromes(char[][] headquarters);
+
+    List<Palindromo> searchPalindromes();
+
+}
