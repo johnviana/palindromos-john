1375110c3c1799707692db05c57cd92bf5f27ef5
johnformata <johnvianaluz@gmail.com>
2024-02-29 13:11:13 -0300

    feat: "refatoracao e exclusao da class"

--- cacapalindromo/src/main/java/com/john/cacapalindromo/core/exception/PalindromeException.java
+++
@@ -1,8 +0,0 @@
-package com.john.cacapalindromo.core.exception;
-
-public class PalindromeException extends RuntimeException {
-
-    public PalindromeException(String message) {
-        super(message);
-    }
-}
