4c2bbb26e8fb78b58fdf24b645a95402943625d5
johnformata <johnvianaluz@gmail.com>
2024-02-28 21:50:22 -0300

    feat: "refatorando  interface para salvarPalindromo"

--- cacapalindromo/src/main/java/com/john/cacapalindromo/core/dataprovider/IncluirPalindrome.java
+++
@@ -1,8 +0,0 @@
-package com.john.cacapalindromo.core.dataprovider;
-
-import com.john.cacapalindromo.core.domain.Palindrome;
-
-public interface IncluirPalindrome {
-
-    void incluir(Palindrome palindrome);
-}
---
+++ cacapalindromo/src/main/java/com/john/cacapalindromo/core/dataprovider/PalindromoDataProvider.java
@@ -0,0 +1,10 @@
+package com.john.cacapalindromo.core.dataprovider;
+
+import com.john.cacapalindromo.core.domain.Palindromo;
+
+import java.util.List;
+
+public interface PalindromoDataProvider {
+
+    void salvarPalindromos(List<Palindromo> palindromos);
+}
