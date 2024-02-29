16cbf650caa0bd9af04a66ce44e23597ee5f7c0b
johnformata <johnvianaluz@gmail.com>
2024-02-29 13:03:41 -0300

    feat: "response de saida criada"

---
+++ cacapalindromo/src/main/java/com/john/cacapalindromo/entrypoint/response/PalindromeResponse.java
@@ -0,0 +1,16 @@
+package com.john.cacapalindromo.entrypoint.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+/**
+ * @author john
+ */
+@Getter
+@Setter
+@ToString
+public class PalindromeResponse {
+
+    private String word;
+
+}
